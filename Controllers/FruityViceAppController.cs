206e412ff47c3a25d39138392b0c415c4a772bdf
Amitesh Surwar <amitesh.surwar1@gmail.com>
2023-04-30 14:17:08 +0530

    Removed Unwanted Files

--- Controllers/FruityViceAppController.cs
+++ Controllers/FruityViceAppController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FruityViceApiIntegerationApp.Interfaces;
+using FruityViceApiIntegerationApp.Models.ResponseModels;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FruityViceApiIntegerationApp.Controllers
@@ -8,5 +10,48 @@ namespace FruityViceApiIntegerationApp.Controllers
     public class FruityViceAppController : ControllerBase
     {
 
+        private readonly IGetAllFruits _getAllFruits;
+        private readonly IGetFruitsByGenus _getFruitsByGenus;
+
+        public FruityViceAppController(
+            IGetAllFruits getAllFruits,
+            IGetFruitsByGenus getFruitsByGenus
+            )
+        {
+            _getAllFruits = getAllFruits;
+            _getFruitsByGenus = getFruitsByGenus;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<AllFruitsResponse>> GetAllFruits()
+        {
+            try
+            {
+                AllFruitsResponse response = await _getAllFruits.GetAllFruitsAsync();
+                return Ok(response);
+
+            }catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<GetFruitsByGenusFinalResponse>> GetFruitsByGenus(string family)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(family))
+                {
+                    return BadRequest("Please enter a Fruit Family");
+                }
+                GetFruitsByGenusFinalResponse response = await _getFruitsByGenus.GetFruitsByFamilyAsync(family);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
     }
 }
